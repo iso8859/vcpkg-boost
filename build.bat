2ca522a31de2e7418030c5ce1e90bfdeb40261d3
Remi THOMAS <remi@xtware.com>
2018-06-13 15:31:18 +0200

    .

--- build.bat
+++ build.bat
@@ -4,12 +4,9 @@ md x86
 cd x86
 set triplet=-DVCPKG_TARGET_TRIPLET=x86-windows-static
 cmake.exe %vcpkg% %msvc% %triplet% -G "Visual Studio 15 2017" -T v140 ..\src
-rem cmake.exe --build .
 cd ..
 md x64
 cd x64
 set triplet=-DVCPKG_TARGET_TRIPLET=x64-windows-static
 cmake.exe %vcpkg% %msvc% %triplet% -G "Visual Studio 15 2017 Win64" -T v140 ..\src
-rem cmake.exe --build .
 cd ..
-:end
