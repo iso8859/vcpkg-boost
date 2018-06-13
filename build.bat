488a8bcd422757be6d37ce05bc18bf19e71d72b2
Remi THOMAS <remi@xtware.com>
2018-06-13 15:57:49 +0200

    Some small updates

--- build.bat
+++ build.bat
@@ -1,12 +1,13 @@
 set vcpkg=-DCMAKE_TOOLCHAIN_FILE=D:/dev/vcpkg/scripts/buildsystems/vcpkg.cmake
 set msvc=-DMSVC=TRUE
+set Version=-T v141
 md x86
 cd x86
 set triplet=-DVCPKG_TARGET_TRIPLET=x86-windows-static
-cmake.exe %vcpkg% %msvc% %triplet% -G "Visual Studio 15 2017" -T v140 ..\src
+cmake.exe %vcpkg% %msvc% %triplet% -G "Visual Studio 15 2017" %Version% ..\src
 cd ..
 md x64
 cd x64
 set triplet=-DVCPKG_TARGET_TRIPLET=x64-windows-static
-cmake.exe %vcpkg% %msvc% %triplet% -G "Visual Studio 15 2017 Win64" -T v140 ..\src
+cmake.exe %vcpkg% %msvc% %triplet% -G "Visual Studio 15 2017 Win64" %Version% ..\src
 cd ..
